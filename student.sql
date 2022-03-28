335b45c29c159e97a8cda516ef4bdc9cca731970
ChangFeng23 <95690002+ChangFeng23@users.noreply.github.com>
2022-03-28 12:14:38 +0800

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM db1.student
+LIMIT 0, 1000
+
+-- Date: 2022-03-28 11:59
+*/
+INSERT INTO `` (`id`,`student_name`,`department`) VALUES (201507,'Guo Yimeng','Software Engineering');
+INSERT INTO `` (`id`,`student_name`,`department`) VALUES (201510,'Chang Feng','Software Engineering');
+INSERT INTO `` (`id`,`student_name`,`department`) VALUES (201526,'Wang Dong','Computer Science');
